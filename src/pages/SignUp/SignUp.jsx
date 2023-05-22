d8442d287d5a6e060077ce4bf51299003305ea4c
tiyasa-tagore <tiyasatagore1999@gmail.com>
2023-05-23 01:52:17 +0600

    googlelogin

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
-import img from '../../assets/img/signup.jpeg'
+import img from '../../assets/img/signup1.jpeg'
 import { AuthContext } from "../../Providers/AuthProvider";
 import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
 
 const SignUp = () => {
 
-    const { createUser } = useContext(AuthContext);
+    const { createUser, googleSignIn } = useContext(AuthContext);
 
     const handleSignUp = event => {
         event.preventDefault();
@@ -28,7 +29,7 @@ const SignUp = () => {
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
                 <div className="w-1/2 mr-12">
-                    <img src={img} alt="" />
+                    <img src={img} width={800} alt="" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
@@ -59,6 +60,8 @@ const SignUp = () => {
                                 <input className="btn btn-primary" type="submit" value="Sign Up" />
                             </div>
                         </form>
+                        <p className='my-4 text-center'>New Here??<Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link> </p>
+                        <button  className='mt-2 p-2 text-center'><FaGoogle style={{fontSize: '2rem'}}></FaGoogle><span onClick={googleSignIn }> Sign in with Google</span></button>
                         <p className='my-4 text-center'>Already Have an Account? <Link className='text-orange-600 font-bold' to="/login">Login</Link> </p>
                     </div>
                 </div>
