f7e84d253e377c890e8ce7f8f45566b81f9933af
tiyasa-tagore <tiyasatagore1999@gmail.com>
2023-05-23 01:44:04 +0600

    login

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,19 +1,28 @@
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
+//google
+const provider = new GoogleAuthProvider();
 
 // eslint-disable-next-line react/prop-types
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null)
 
     const createUser = (email, password) => {
         setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const userUpdate = (currentUser, displayName, photoURL) => {
+        return updateProfile(currentUser, {
+            displayName: displayName,
+            photoURL: photoURL,
+        })
     }
 
     const signIn = (email, password) => {
@@ -21,10 +30,34 @@ const AuthProvider = ({ children }) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
+    const googleSignIn = () => {
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const user = result.user;
+                setUser(user)
+            })
+            .catch(error => {
+                const errorMessage = error.message;
+                setError(errorMessage)
+            })
+    }
+
+   
+
+
     const logOut = () => {
         setLoading(true);
         return signOut(auth);
     }
+    const signOutGoogle = () => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+        // eslint-disable-next-line no-unused-vars
+        }).catch((error) => {
+            // An error happened.
+        });
+    }
+    
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
@@ -38,11 +71,15 @@ const AuthProvider = ({ children }) => {
     }, [])
 
     const authInfo = {
+        googleSignIn,
+        signOutGoogle,
+        userUpdate,
         user,
         loading,
+        error,
         createUser, 
         signIn, 
-        logOut
+        logOut,
     }
 
     return (
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,10 +2,11 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProvider";
 import img from '../../assets/img/login.jpeg'
+import { FaGoogle } from 'react-icons/fa';
 
 const Login = () => {
 
-    const { signIn } = useContext(AuthContext);
+    const { signIn, googleSignIn } = useContext(AuthContext);
 
     const handleLogin = event => {
         event.preventDefault();
@@ -24,7 +25,7 @@ const Login = () => {
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
                 <div className="w-1/2 mr-12">
-                    <img src={img} alt="" />
+                    <img src={img} width={800} className='img-fluid' alt="" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
@@ -49,7 +50,9 @@ const Login = () => {
                                 <input className="btn btn-primary" type="submit" value="Login" />
                             </div>
                         </form>
-                        <p className='my-4 text-center'>New to Car Doctors <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link> </p>
+                        <p className='my-4 text-center'>New Here??<Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link> </p>
+                        <button  className='mt-2 p-2 text-center'><FaGoogle style={{fontSize: '2rem'}}></FaGoogle><span onClick={googleSignIn }> Sign in with Google</span></button>
+                        
                     </div>
                 </div>
             </div>
