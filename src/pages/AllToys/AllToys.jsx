7f01c47985aaab2ad5dd00768a39c66840e5a26b
tiyasa-tagore <tiyasatagore1999@gmail.com>
2023-05-23 17:57:38 +0600

    search

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -3,28 +3,40 @@ import ToyCard from "./ToyCard";
 
 const AllToys = () => {
     const [alltoys, setAlltoys] = useState([]);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
         fetch('cars.json')
             .then(res => res.json())
             .then(data => setAlltoys(data));
     }, [])
+
+    const filteredToys = alltoys.filter(toy => 
+        toy.name.toLowerCase().includes(search.toLowerCase())
+      ).slice(0, 20);
+
     return (
-        <div className="mt-4">
+        <div>
+            <div className="mt-4">
             <div className="text-center">
                 
                 <h2 className="text-5xl">ALL TOYS </h2>
                 
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                {
-                    alltoys.map(alltoy => <ToyCard
-                        key={alltoy._id}
-                        alltoy={alltoy}
-                    ></ToyCard>)
-                }
+            {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"> */}
+            <div>
+      <input 
+        type="text" 
+        placeholder="Search toys..." 
+        onChange={e => setSearch(e.target.value)}
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">{filteredToys.map((toy) => (
+        <ToyCard key={toy.id} toy={toy} />
+      ))} </div>
+      
             </div>
-            </div>
+        </div>
+        </div>
     );
 };
 
--- src/pages/AllToys/ToyCard.jsx
+++ src/pages/AllToys/ToyCard.jsx
@@ -1,8 +1,30 @@
+import { Link } from "react-router-dom";
 
-const ToyCard = () => {
+// eslint-disable-next-line react/prop-types
+const ToyCard = ({ toy }) => {
+    // eslint-disable-next-line react/prop-types
+    const { _id, name, sellerName, subCategory, price, availableQuantity, description, pictureUrl } = toy;
+
+
+    
     return (
-        <div>
-            
+        <div className="card w-96 bg-base-100 shadow-xl lg:grid-cols-3 md:grid-cols-2">
+            <figure className="px-10 pt-10">
+                <img src={pictureUrl} alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">Car name: {name}</h2>
+                <h2 className="card-title">Seller: {sellerName}</h2>
+                <h2 className="card-title">Sub-Category: {subCategory}</h2>
+                <h2 className="card-title"> Quantity:{availableQuantity}</h2>
+                <h2 className="card-title">Description: {description}</h2>
+                <p className="text-xl text-orange-500">Price: ${price}</p>
+                <div className="card-actions">
+                    <Link to={`/addatoy/${_id}`}>
+                        <button className="btn btn-primary">Book Now</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
